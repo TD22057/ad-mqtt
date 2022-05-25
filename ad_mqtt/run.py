873b50182a716630306ae236d8eb820c48578bf3
Santiago Soto <santiagosoto8@me.com>
2022-05-25 14:27:45 -0300

    Refactor logger, fewer lines, timestamps adopt ISO-8601, imports to lowercase

--- ad_mqtt/run.py
+++ ad_mqtt/run.py
@@ -1,53 +1,49 @@
 import logging
-import alarmdecoder as AD
-import insteon_mqtt as IM
+import alarmdecoder as ad
+import insteon_mqtt as mqtt
 from .Bridge import Bridge
 from .Client import Client
 from .Discovery import Discovery
 
 
-def run(ad_host, ad_port,mqtt_broker,mqtt_port,mqtt_user,mqtt_pass,mqtt_id, alarm_code, zone_data,
+def run(ad_host, ad_port, mqtt_broker, mqtt_port, mqtt_user, mqtt_pass, mqtt_id, alarm_code, zone_data,
         log_level, log_screen=True, log_file=None):
-    fmt = '%(asctime)s.%msecs)03d %(levelname)s %(module)s: %(message)s'
-    datefmt = '%Y-%m-%d %H:%M:%S'
-    formatter = logging.Formatter(fmt, datefmt)
+
     if log_screen:
-        screen_handler = logging.StreamHandler()
-        screen_handler.setFormatter(formatter)
+        log_handler = logging.StreamHandler()
     if log_file:
-        file_handler = logging.handlers.WatchedFileHandler(log_file)
-        file_handler.setFormatter(formatter)
+        log_handler = logging.handlers.WatchedFileHandler(log_file)
 
     log_names = ["ad_mqtt"]
 
     for name in log_names:
+        logging.basicConfig(format='%(asctime)s %(message)s')
         log = logging.getLogger(name)
         log.setLevel(logging.getLevelName(log_level))
-        if log_screen:
-            log.addHandler(screen_handler)
-        if log_file:
-            log.addHandler(file_handler)
+
+        if log_screen or log_file:
+            log.addHandler(log_handler)
+
     # Alarm decoder network device.
     ad_client = Client(ad_host, ad_port)
-    decoder = AD.AlarmDecoder(ad_client)
+    decoder = ad.AlarmDecoder(ad_client)
     decoder._wire_events()
 
-    #MQTT Config
-    mqtt_client = IM.network.Mqtt()
+    # MQTT Config
+    mqtt_client = mqtt.network.Mqtt()
     mqtt_client.load_config(config={
         'broker': mqtt_broker,
-        'port':mqtt_port,
-        'username':mqtt_user,
-        'password':mqtt_pass,
+        'port': mqtt_port,
+        'username': mqtt_user,
+        'password': mqtt_pass,
         'id': mqtt_id,
         'availability_topic': "alarm/available"
     })
 
-
     bridge = Bridge(mqtt_client, decoder, zone_data, alarm_code)
-    discovery = Discovery(mqtt_client, bridge, zone_data)
+    Discovery(mqtt_client, bridge, zone_data)
 
-    loop = IM.network.poll.Manager()
+    loop = mqtt.network.poll.Manager()
     loop.add(ad_client, connected=False)
     loop.add(mqtt_client, connected=False)
 
--- run.py
+++ run.py
@@ -1,23 +1,24 @@
 #!./venv/bin/python
 
+from email.policy import default
 import logging
 import sys
-sys.path.insert( 0, "." )
-import ad_mqtt.run as AD2MQTT
+
+sys.path.insert(0, ".")
+import ad_mqtt.run as ad2mqtt
 from decouple import config
 
 ad_host = config('AD_HOST', default="127.0.0.1")
-ad_port = config('AD_PORT',default=10000, cast=int)
+ad_port = config('AD_PORT', default=10000, cast=int)
 mqtt_broker = config('MQTT_BROKER', default="127.0.0.1")
-mqtt_port = config('MQTT_PORT',default=1883, cast=int)
-mqtt_user = config('MQTT_USER',default="")
-mqtt_pass = config('MQTT_PASS',default="")
-mqtt_id = config('MQTT_ID',default="ad-mqtt")
-log_level = config('LOG_LEVEL',default=logging.ERROR)
-log_screen = config('LOG_SCREEN',default=False, cast=bool)
-log_file = config('LOG_FILE',default="log.txt")
-
+mqtt_port = config('MQTT_PORT', default=1883, cast=int)
+mqtt_user = config('MQTT_USER', default="")
+mqtt_pass = config('MQTT_PASS', default="")
+mqtt_id = config('MQTT_ID', default="ad-mqtt")
+log_level = config('LOG_LEVEL', default=logging.DEBUG)
+log_screen = config('LOG_SCREEN', default=True, cast=bool)
+log_file = config('LOG_FILE', default=None)
 alarm_code = config('ALARM_CODE', default="1234")
 
-AD2MQTT.run(ad_host, ad_port, mqtt_broker,mqtt_port,mqtt_user,mqtt_pass,mqtt_id,alarm_code, zone_data,
-                log_level=log_level, log_screen=log_screen, log_file=log_file)
+ad2mqtt.run(ad_host, ad_port, mqtt_broker, mqtt_port, mqtt_user, mqtt_pass, mqtt_id, alarm_code, zone_data,
+            log_level=log_level, log_screen=log_screen, log_file=log_file)
